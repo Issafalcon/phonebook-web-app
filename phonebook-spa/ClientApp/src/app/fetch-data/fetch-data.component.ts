453bba43244c9ce575bc9e034472aa18578beb1a
Adam Figgins <adam.figgins@gmail.com>
2023-03-04 14:40:30 +0000

    fix(fetch-data): Updating Angular app to fetch phonebook entries
    - Allows request to match the get endpoint of the API

--- phonebook-spa/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ phonebook-spa/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,23 +1,20 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { PhoneBookEntry } from './models/phoneBookEntry';
 
 @Component({
   selector: 'app-fetch-data',
-  templateUrl: './fetch-data.component.html'
+  templateUrl: './fetch-data.component.html',
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[] = [];
+  public entries: PhoneBookEntry[] = [];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+    http.get<PhoneBookEntry[]>(baseUrl + 'phonebook').subscribe(
+      (result) => {
+        this.entries = result;
+      },
+      (error) => console.error(error)
+    );
   }
-}
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
 }
---
+++ phonebook-spa/ClientApp/src/app/fetch-data/models/phoneBookEntry.ts
@@ -0,0 +1,5 @@
+export interface PhoneBookEntry {
+  firstName: string;
+  lastName: number;
+  phoneNumber: number;
+}
